4ee0e6055a3e3539fffa4f04543491a9e2c8d7a3
Rafael Licett <rafael.licett@mentum.group>
2024-03-08 21:35:46 +0000

    change: ajust sync item by allow create price list of other currency

--- gp_phonix_integration/gp_phonix_integration/service/utils.py
+++ gp_phonix_integration/gp_phonix_integration/service/utils.py
@@ -1,4 +1,18 @@
 import frappe
+from qp_phonix_front.qp_phonix_front.uses_cases.shipping_method.shipping_method_list import __get_customer
+
 def get_master_setup(master_name):
 
     return frappe.get_doc("qp_GP_MasterSetup", master_name)
+
+def get_price_list(customer = None):
+    
+    if not customer:
+        
+        customer = __get_customer()
+
+    if customer.default_price_list:
+        
+        return customer.default_price_list
+
+    return frappe.db.get_single_value("Selling Settings", "selling_price_list")
