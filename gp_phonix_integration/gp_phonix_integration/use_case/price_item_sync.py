3ed8c1c488af499c876a65543fabd7ce80d04078
Rafael Licett <rafael.licett@mentum.group>
2026-03-13 15:41:01 +0000

    [add] price list sync add validate from

--- gp_phonix_integration/gp_phonix_integration/use_case/price_item_sync.py
+++ gp_phonix_integration/gp_phonix_integration/use_case/price_item_sync.py
@@ -11,8 +11,8 @@ from frappe.utils import now
 
 from gp_phonix_integration.gp_phonix_integration.service.item_sync import get_items_and_price_list, create_item_sync_log, execute_sync_items, update_item_sync_log, get_count_row, exist_item_sync_log_pending
 from qp_phonix_front.qp_phonix_front.uses_cases.flete.update_price_list import handler as update_flete_price_list
-@frappe.whitelist()
 
+@frappe.whitelist()
 def handler(master_name, store_main = None):
 
     if not exist_item_sync_log_pending():
@@ -71,6 +71,7 @@ def price_list_add(price_level):
         buying,
         price_not_uom_dependent,
         qp_without_discount,
+        valid_from,
         modified, 
         creation, 
         modified_by, 
@@ -81,7 +82,7 @@ def price_list_add(price_level):
         src.p_name,
         src.p_name,
         src.p_curr,
-        1, 1, 0, 0, 0,
+        1, 1, 0, 0, 0, '2024-12-31'
         NOW(), NOW(),
         'Administrator', 'Administrator', 0
     FROM (
@@ -89,7 +90,6 @@ def price_list_add(price_level):
         UNION SELECT '{price_level} USD', 'USD'
         UNION SELECT '{price_level} EUR', 'EUR'
     ) AS src
-    -- Validamos contra la tabla real para ver si ya existe el nombre
     LEFT JOIN `tabPrice List` AS target ON src.p_name = target.name
     WHERE target.name IS NULL;
     """
@@ -138,9 +138,7 @@ def price_item_add(price_level):
         UNION ALL SELECT '{price_level} EUR' 
         UNION ALL SELECT '{price_level} USD'
     ) AS t_list
-    -- Un INNER JOIN asegura que el Item exista en el sistema
     INNER JOIN `tabItem` AS item ON line.id_item = item.item_code
-    -- Validamos que NO exista ya ese nombre (Item:Lista)
     LEFT JOIN `tabItem Price` AS existing_price 
         ON existing_price.name = CONCAT(line.id_item, ':', t_list.list_name)
     WHERE 
